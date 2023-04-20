9303db9e0732221dc1c3ef1008c38e465b31b9f4
ahsouza <annibalhsouza@gmail.com>
2023-04-20 06:33:42 -0300

    chore: init bsconf script perl

---
+++ lib/BSconf/Utils.pm
@@ -0,0 +1,38 @@
+package BSconf::Utils;
+
+#           Copyright (C) 2021 - 2023, Barca, Inc. 
+#    Email: <opensource@barca.com>  GitHub: @BarcaWebCloud. 
+#    Project: BSCONF to install and configure web servers, games server, email server and more
+ 
+ our $VERSION = 0.1.0;
+ 
+ =head1 NAME
+ 
+ BSconf::Utils - some useful command-line utilities
+ 
+ =head1 DESCRIPTION
+ 
+ A Perl Script That Installs, Configures And Repairs Servers in General
+ 
+ F<bsconf> - print a date-time tag in form C<YYYY-MM-DD-hhmmss>
+ 
+ 
+ =head1 AUTHOR
+ 
+ Barca Web Cloud opensource@barca.com https://cloud.barca.com/
+ 
+ =head1 INSTALLATION
+ 
+ Using C<cpan>:
+ 
+  $ cpan BSconf::Utils
+ 
+ Manual install:
+ 
+  $ perl Makefile.PL
+  $ make
+  $ make install
+ 
+ =cut
+ 
+ 1;
